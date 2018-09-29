7ba8aa9c6698f09ebc21fc20e9e5326a5aa6bcee
helloyeops <helloyeops@helloyeopsui-MacBook-Pro.local>
2018-09-29 16:56:40 +0900

    urllib -> requests 패키지로 변경

--- getRTMSDataSvcAptTrade.py
+++ getRTMSDataSvcAptTrade.py
@@ -1,13 +1,27 @@
-import urllib.request
+# 서비스명 : 아파트매매 실거래자료
+
+import requests
 import json
 
+# 국토부 API END POINT
 API_HOST = 'http://openapi.molit.go.kr:8081'
-APP_KEY = '%2FP%2FIlllk9vLZCORgEzCUfeDjf5tjElZFhAVORx%2B%2BetX1vB4tcrMFhOMQ7aKmjXoGKy5rLZ6gfBRKjX4iuoRNzQ%3D%3D'
-
+API_SERVICE = '/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade'
+API_SERVICE_KEY = '/P/Illlk9vLZCORgEzCUfeDjf5tjElZFhAVORx++etX1vB4tcrMFhOMQ7aKmjXoGKy5rLZ6gfBRKjX4iuoRNzQ=='
 # LAWD_CD=41135 (분당구 전체)
-req = urllib.request.urlopen(API_HOST + '/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=41135&DEAL_YMD=201801&_type=json&serviceKey=' + APP_KEY)
-res = req.read()
+LAWD_CD = 41135
+RES_TYPE = 'json'
 
+params = {
+  'LAWD_CD': LAWD_CD,
+  'DEAL_YMD': '201801',
+  '_type': RES_TYPE,
+  'serviceKey': API_SERVICE_KEY,
+}
+
+req = requests.get(API_HOST + API_SERVICE, params=params)
+print(req.url)
+
+res = req.text
 res_json = json.loads(res)
 
 print(json.dumps(res_json, indent=2, ensure_ascii=False))
