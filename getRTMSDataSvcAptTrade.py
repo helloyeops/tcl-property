b5d3117942ff02bd487e1c8e968d638eabe7f8be
helloyeops <helloyeops@helloyeopsui-MacBook-Pro.local>
2018-09-30 20:21:07 +0900

    이번달 지난달 data 가져오도록 추가

--- getRTMSDataSvcAptTrade.py
+++ getRTMSDataSvcAptTrade.py
@@ -1,20 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[21]:
-
-
 # 서비스명 : 아파트매매 실거래자료
 # 제공    : 공공데이터포털
 
 import requests
 import json
 import pandas as pd
-
-
-# In[3]:
-
-
+import datetime
 
 # 국토부 API END POINT
 API_HOST = 'http://openapi.molit.go.kr:8081'
@@ -23,55 +13,30 @@ API_SERVICE_KEY = '/P/Illlk9vLZCORgEzCUfeDjf5tjElZFhAVORx++etX1vB4tcrMFhOMQ7aKmj
 LAWD_CD = 41135 # 분당구 전체
 RES_TYPE = 'json'
 
-params = {
-  'LAWD_CD': LAWD_CD,
-  'DEAL_YMD': '201801',
-  '_type': RES_TYPE,
-  'serviceKey': API_SERVICE_KEY,
-}
+dealYmds = []
+for i in range(0, 2):
+  now = datetime.datetime.now()
+  now = now.replace(month=now.month - i)
+  dealYmd = now.strftime("%Y%m")
+  dealYmds.append(dealYmd)
 
-print(params)
+for dealYmd in dealYmds:
+  params = {
+    'LAWD_CD': LAWD_CD,
+    'DEAL_YMD': dealYmd,
+    '_type': RES_TYPE,
+    'serviceKey': API_SERVICE_KEY,
+  }
 
+  req = requests.get(API_HOST + API_SERVICE, params=params)
+  res = req.text
 
-# In[4]:
+  res_json = json.loads(res)
+  data = res_json['response']['body']['items']['item']
 
+  df = pd.DataFrame.from_dict(data)
+  df['거래금액'] = df['거래금액'].str.replace(',', '')
+  df['거래금액'] = df['거래금액'].astype('int64')
+  df['법정동'] = df['법정동'].str.strip()
 
-req = requests.get(API_HOST + API_SERVICE, params=params)
-
-print(req.url)
-
-
-# In[20]:
-
-
-res = req.text
-res_json = json.loads(res)
-
-print(res_json['response']['body']['items']['item'])
-
-
-# In[78]:
-
-
-data = res_json['response']['body']['items']['item']
-
-df = pd.DataFrame.from_dict(data)
-# df
-
-
-# In[79]:
-
-
-df['거래금액'] = df['거래금액'].str.replace(',', '')
-df['거래금액'] = df['거래금액'].astype('int64')
-df['법정동'] = df['법정동'].str.strip()
-
-print(df.dtypes)
-
-# In[80]:
-
-
-df.to_csv("data/filename.csv", index=False)
-
-
-# In[ ]:
+  df.to_csv("data/" + dealYmd + "_deal.csv", index=False)
